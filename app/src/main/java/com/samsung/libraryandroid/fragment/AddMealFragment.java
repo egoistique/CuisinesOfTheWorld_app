7e43a92fde1635d3076e0dee0b5823c9f6cc8aaa
katya <katya@example.ru>
2022-05-23 19:39:47 +0300

    first commit

---
+++ app/src/main/java/com/samsung/libraryandroid/fragment/AddMealFragment.java
@@ -0,0 +1,22 @@
+package com.samsung.libraryandroid.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.samsung.libraryandroid.R;
+
+public class AddMealFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_add_meal, container, false);
+
+        return view;
+    }
+}
